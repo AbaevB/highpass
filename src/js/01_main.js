165c54813fbad34c0138b1f261ce923f225b2332
AbaevB <abaev70@hotmail.com>
2024-10-20 03:37:44 +0700

    add media.scss

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,13 +1,45 @@
-document.addEventListener('DOMContentLoaded',function(){
+//document.addEventListener('DOMContentLoaded',function(){
+//  let headerBtn = document.querySelector(".header__search-btn");
+//  let headerForm = document.querySelector(".header__search-form");
+//  let formClose = document.querySelector(".search-form__close-btn");
+//
+//
+//    headerBtn.addEventListener('click', function(){
+//      headerForm.classList.remove('d-none');
+//    });
+//    formClose.addEventListener('click', function(){
+//      headerForm.classList.add('d-none');
+//    });
+//});
+
+document.addEventListener('DOMContentLoaded', function () {
   let headerBtn = document.querySelector(".header__search-btn");
   let headerForm = document.querySelector(".header__search-form");
   let formClose = document.querySelector(".search-form__close-btn");
 
+  headerBtn.addEventListener('click', function () {
+    // Скрываем кнопка и показываем форму с анимацией
+    gsap.to(headerForm, {
+      duration: 0.5,
+      y: 0,
+      opacity: 1,
+      display: "block",
+      onStart: () => { headerBtn.style.display = 'none'; },
+      onComplete: () => { headerForm.classList.remove('d-none'); }
+    });
+  });
 
-    headerBtn.addEventListener('click', function(){
-      headerForm.classList.remove('d-none');
+  formClose.addEventListener('click', function () {
+    // Плавно скрываем форму и показываем кнопку
+    gsap.to(headerForm, {
+      duration: 0.5,
+      y: -50,
+      opacity: 0,
+      onStart: () => { headerForm.classList.remove('d-none'); },
+      onComplete: () => {
+        headerForm.style.display = 'none';
+        headerBtn.style.display = 'block';
+      }
     });
-    formClose.addEventListener('click', function(){
-      headerForm.classList.add('d-none');
-    });
+  });
 });
